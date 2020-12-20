958539e1ebc390463b44f317f88dc65c5ede620a
Johan Castillo <jcjohan2707@gmail.com>
2020-12-20 17:08:22 -0400

    Variables

---
+++ 3.variables.py
@@ -0,0 +1,20 @@
+# Declaration of the variable
+name = "Johan"
+
+# Print in the console
+print(name)
+
+# Multiple declaration of variable in one line
+x, book = 100, "I Robot"
+
+# Conventions
+book_name = None # Snake Case
+bookName = None # Camel Case
+BookName = None # Pascal Case
+
+# Conventions for values constants
+PI = 3.1416
+
+# Redeclaration of variable
+user = "Peter"
+user = "Mary"
